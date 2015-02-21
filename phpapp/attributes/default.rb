4ac9a52d50bb66a8e6ed0ebc08b47a000442e8c0
Raja S <raja.s@changepond.com>
2015-02-22 01:45:23 +0530

    update

---
+++ phpapp/attributes/default.rb
@@ -0,0 +1,12 @@
+#image in the assets folder
+default["phpapp"]["imgdir"] = '/var/www/img/'
+#Sound in the assets folder
+default["phpapp"]["sounddir"] = '/var/www/sound/'
+#image in the assets folder
+default["phpapp"]["img_url"] = '/var/www/img/'
+#Sound in the assets folder
+default["phpapp"]["sound_url"] = '/var/www/sound/'
+#Document root for dev app
+default["phpapp"]["docroot"] = 'srv/www/immweb/app/www/'
+#server dns
+default["phpapp"]["stageserverurl"] = 'stage.immaws.com'
